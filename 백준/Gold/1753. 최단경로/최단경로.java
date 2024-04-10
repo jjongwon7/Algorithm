1597e58ca96a2964a91aa1afef015d4f794e7c01
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-04-11 01:42:08 +0900

    [Gold IV] Title: 최단경로, Time: 984 ms, Memory: 110036 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,34 +27,31 @@ public class Main {
             int w = Integer.parseInt(st.nextToken()); // 가중치
 
             arrayLists.get(start).add(new Node(end, w));
-        }        int[] dist = new int[v + 1];
-        boolean[] visited = new boolean[v + 1];
+        }
 
+        int[] dist = new int[v + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[startV] = 0;
 
         // 다익스트라 알고리즘 (음의 가중치가 없으므로, 적용 가능)
-        for (int i = 0; i < v; i++) {
-            int nodeValue = Integer.MAX_VALUE;
-            int nodeIdx = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(startV, dist[startV]));
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            int curV = curNode.v;
+            int curW = curNode.w;
 
-            // 방문하지 않은 노드 중 최소 길이를 탐색한다.
-            for (int j = 1; j <= v; j++) {
-                if (!visited[j] && dist[j] < nodeValue) {
-                    nodeValue = dist[j];
-                    nodeIdx = j;
-                }
+            // 우선순위 큐에 넣다보면, (2, 12), (2, 11) 처럼 도중에 최소 길이가 갱신되는 경우가 있음.
+            // 이런 경우는 고려할 필요 없으니, continue
+            if (curW > dist[curV]) {
+                continue;
             }
 
-            // 현재 최소 길이를 지닌 노드를 방문한다.
-            visited[nodeIdx] = true;
-
-            // 방문한 노드 인근 dist 초기화한다.
-            for (Node node : arrayLists.get(nodeIdx)) {
-                int endNode = node.v;
-                int w = node.w;
-                if (dist[endNode] > dist[nodeIdx] + w) {
-                    dist[endNode] = dist[nodeIdx] + w;
+            // 연결된 간선 순회하며, 갱신할 수 있는 것들은 갱신하고 우선순위 큐에 offer
+            for (Node node : arrayLists.get(curV)) {
+                if (dist[node.v] > curW + node.w) {
+                    dist[node.v] = curW + node.w;
+                    pq.offer(new Node(node.v, dist[node.v]));
                 }
             }
         }
@@ -67,13 +65,18 @@ public class Main {
         }
     }
 
-    static class Node {
+    static class Node implements Comparable<Node> {
         int v; // 도착 노드
         int w; // 가중치
 
         public Node(int v, int w) {
             this.v = v;
             this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            return this.w - n.w;
         }
     }
 }
