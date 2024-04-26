70ad51592cb6a9fa805341857b7f3c04a89a9b14
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-04-27 00:22:32 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        dp = new int[N+1];
-        System.out.println(cal(N));
-    }
+        int n = Integer.parseInt(br.readLine());
+        int[] mem = new int[n + 1];
+        Arrays.fill(mem, Integer.MAX_VALUE);
 
-    static int cal(int N) {
-        if(N <= 1)
-            return 0;
-        if(dp[N] == 0) {
-            if ((N % 6) == 0)
-                dp[N] = Math.min(cal(N / 3), Math.min(cal(N / 2), cal(N - 1))) + 1;
-            else if ((N % 3) == 0)
-                dp[N] = Math.min(cal(N / 3), cal(N - 1)) + 1;
-            else if ((N % 2) == 0)
-                dp[N] = Math.min(cal(N / 2), cal(N - 1)) + 1;
-            else
-                dp[N] = cal(N - 1) + 1;
+        mem[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (i % 3 == 0) {
+                mem[i] = Math.min(mem[i], mem[i / 3]);
+            }
+            if (i % 2 == 0) {
+                mem[i] = Math.min(mem[i], mem[i / 2]);
+            }
+            mem[i] = Math.min(mem[i], mem[i - 1]);
+            mem[i]++;
         }
-        return dp[N];
+
+        System.out.println(mem[n]);
     }
 }
