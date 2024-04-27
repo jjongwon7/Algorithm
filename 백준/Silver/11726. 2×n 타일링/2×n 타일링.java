cf4eda7e61237c39e51f889f0b2721cf9f572b37
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-04-27 16:16:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -3,20 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        dp = new int[n+1];
-        System.out.println(cal(n));
-    }
-    static int cal(int n) {
-        if(n == 1)
-            return 1;
-        if(n == 2)
-            return 2;
-        if(dp[n] == 0)
-            dp[n] = (cal(n-1) + cal(n-2)) % 10_007;
-        return dp[n];
+
+        int[] mem = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                mem[1] = 1;
+            } else if (i == 2) {
+                mem[2] = 2;
+            } else {
+                mem[i] = mem[i - 1] % 10007 + mem[i - 2] % 10007;
+                mem[i] %= 10007;
+            }
+        }
+
+        System.out.println(mem[n]);
     }
 }
