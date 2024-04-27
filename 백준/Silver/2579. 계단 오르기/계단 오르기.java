26dd592263ecbd0fa67e03588e0436d05b43c27e
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-04-27 16:45:07 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] scores = new int[n + 1];
-        int[][] mem = new int[n + 1][3];
+        int[] mem = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
             scores[i] = Integer.parseInt(br.readLine());
@@ -15,17 +15,14 @@ public class Main {
 
         for (int i = 1; i <= n; i++) {
             if (i == 1) {
-                mem[1][1] = scores[1];
-                mem[1][2] = 0;
+                mem[1] = scores[1];
             } else if (i == 2) {
-                mem[2][1] = scores[2];
-                mem[2][2] = scores[1] + scores[2];
+                mem[2] = scores[1] + scores[2];
             } else {
-                mem[i][1] = Math.max(mem[i - 2][1], mem[i - 2][2]) + scores[i];
-                mem[i][2] = mem[i - 1][1] + scores[i];
+                mem[i] = Math.max(mem[i - 3] + scores[i - 1], mem[i - 2]) + scores[i];
             }
         }
 
-        System.out.println(Math.max(mem[n][1], mem[n][2]));
+        System.out.println(mem[n]);
     }
 }
