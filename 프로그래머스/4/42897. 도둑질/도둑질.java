e6b59287135e5f7d94ee2bf0c5370752299fa607
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-08-31 11:56:39 +0900

    [level 4] Title: 도둑질, Time: 25.93 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,18 +1,19 @@
 class Solution {
     public int solution(int[] money) {
-        int[] dpO = new int[money.length];
-        int[] dpX = new int[money.length];
+        int n = money.length;
+        int[] firstO = new int[n];
+        int[] firstX = new int[n];
+        firstO[1] = firstO[0] = money[0];
+        firstX[1] = money[1];
         
-        dpO[0] = money[0];
-        dpO[1] = money[0];
-        dpX[0] = 0;
-        dpX[1] = money[1];
-        
-        for(int i=2; i<money.length; i++) {
-            dpO[i] = Math.max(dpO[i-1], dpO[i-2] + money[i]);
-            dpX[i] = Math.max(dpX[i-1], dpX[i-2] + money[i]);
+        for(int i=2; i<n-1; i++) {
+            firstO[i] = Math.max(firstO[i-1], firstO[i-2] + money[i]);
         }
         
-        return Math.max(dpO[money.length-2], dpX[money.length-1]);
+        for(int i=2; i<n; i++) {
+            firstX[i] = Math.max(firstX[i-1], firstX[i-2] + money[i]);
+        }
+        
+        return Math.max(firstO[n - 2], firstX[n - 1]);
     }
 }
