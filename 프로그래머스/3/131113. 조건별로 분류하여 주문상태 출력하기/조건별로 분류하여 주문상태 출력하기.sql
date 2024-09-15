b782115e039fba07ee820da565682cabe62ca81d
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-09-15 21:05:30 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+    CASE
+        WHEN OUT_DATE IS NULL
+        THEN '출고미정'
+        WHEN (YEAR(OUT_DATE) <= 2022 AND MONTH(OUT_DATE) < 5) OR
+             (YEAR(OUT_DATE) = 2022 AND MONTH(OUT_DATE) = 5 AND DAY(OUT_DATE) = 1)
+        THEN '출고완료'
+        ELSE '출고대기'
+    END 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
