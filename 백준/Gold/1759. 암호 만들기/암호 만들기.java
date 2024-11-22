439b766785ac90c627be27ca16fc7ebe8700c47d
jjongwon7 <81209525+jjongwon7@users.noreply.github.com>
2024-11-22 16:34:09 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,68 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int l, c;
-    static char[] ans;
-    static char[] arr;
-    static boolean[] check;
 
-    public static boolean isVaild() {
-        int consonant = 0; // 자음
-        int vowel = 0; // 모음
-        for (int i = 0; i < c; i++) {
-            if (check[i] == true) {
-                char alp = arr[i];
-                if (alp == 'a' || alp == 'e' || alp == 'i' || alp == 'o' || alp == 'u') {
-                    vowel++;
-                } else {
-                    consonant++;
-                }
-            }
-        }
-        if (vowel >= 1 && consonant >= 2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public static void solution(int L, int pre) {
-        if (L == l) {
-            if (isVaild()) {
-                for (char an : ans) {
-                    System.out.print(an);
-                }
-                System.out.println();
-            }
-            return;
-        }
-        for (int i = pre; i < c; i++) {
-            if (check[i] == false) {
-                ans[L] = arr[i];
-                check[i] = true;
-                solution(L + 1, i+1);
-                check[i] = false;
-            }
-        }
-    }
+    final static Set<Character> vowels = new HashSet<>(List.of('a', 'e', 'i', 'o', 'u'));
+    static StringBuilder sb = new StringBuilder();
+    static char[] possibleChars;
+    static boolean[] used;
+    static int l, c;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        l = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        ans = new char[l];
-        arr = new char[c];
-        check = new boolean[c];
+
+        l = Integer.parseInt(st.nextToken()); // 암호를 구성하는 서로 다른 L개의 알파벳
+        c = Integer.parseInt(st.nextToken()); // 암호를 구성하는 문자의 종류
+
+        possibleChars = new char[c];
+        used = new boolean[c];
+
         st = new StringTokenizer(br.readLine());
-        ArrayList<Character> arrayList = new ArrayList<>();
+
         for (int i = 0; i < c; i++) {
-            arr[i] = st.nextToken().charAt(0);
+            possibleChars[i] = st.nextToken().charAt(0);
         }
-        Arrays.sort(arr);
-        solution(0, 0);
+
+        Arrays.sort(possibleChars);
+
+        dfs(0, new StringBuilder(), -1);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, StringBuilder curPassword, int lastIdx) {
+        if (depth == l) {
+            if (isValidPassword(curPassword.toString())) {
+                sb.append(curPassword).append("\n");
+            }
+
+            return;
+        }
+
+        for (int i = lastIdx + 1; i < c; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                curPassword.append(possibleChars[i]);
+                dfs(depth + 1, curPassword, i);
+                used[i] = false;
+                curPassword.deleteCharAt(curPassword.length() - 1);
+            }
+        }
+    }
+
+    private static boolean isValidPassword(String str) {
+        int vowelCount = 0;
+
+        for (int i = 0; i < l; i++) {
+            if (vowels.contains(str.charAt(i))) {
+                vowelCount++;
+            }
+        }
+
+        if (vowelCount >= 1 && l - vowelCount >= 2) {
+            return true;
+        }
+
+        return false;
     }
 }
